17172e56c2460392903c21a4ad385cccdcab6ce9
Michel Allan <michelallangs@gmail.com>
2022-10-13 16:52:22 -0300

    removed js fixing job sizes (using css to fix it instead) and some unnecessary transitions

--- js/application.js
+++ js/application.js
@@ -1,17 +1,7 @@
 var ready;
 
 ready = function() {
-  function adjustJobSize(){
-    var jobW = $(".job").innerWidth();
-
-    $(".job").height(jobW);
-  }
-
-  window.addEventListener('resize', function(event){
-    adjustJobSize();
-  });
-
-  adjustJobSize();
+  
 };
 
 $(document).ready(ready);
